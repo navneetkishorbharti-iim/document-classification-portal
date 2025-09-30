c61e609a44bb6d0fee47b904b330ad36e24e665d
navneetkishorbharti-iim <navneetkishor.bharti@gmail.com>
2025-09-30 14:47:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -201,7 +201,7 @@ if uploaded_file is not None:
             else:
                 st.success(f"The document is classified as a: **{doc_type}**")
             st.write(f"Confidence Score: **{confidence}**")
-            st.subheader("Extracted Text (sample):")
+           # st.subheader("Extracted Text (sample):")
             st.code(text[:1000])
         else:
             st.error("❌ Uploaded file is empty or not a valid PDF, please try again.")
